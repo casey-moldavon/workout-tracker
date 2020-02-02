5335ea6a12198cf55597945c500553a0ec5ee258
Casey Moldavon <cmoldavon@gmail.com>
2020-02-01 19:04:31 -0800

    Adding all files and folders

---
+++ models/workout.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    
+    exercize: {
+        type: Schema.Types.ObjectId,
+        ref: "Exercise"
+    }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
