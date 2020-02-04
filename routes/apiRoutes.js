0730c0ac601190dfcb6a95ee4b07dd6237f596eb
Casey Moldavon <cmoldavon@gmail.com>
2020-02-04 14:25:49 -0800

    Attempting to fix API Routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,13 @@
 const Exercise = require("../models/exercise");
 const Workout = require("../models/workout");
-
+// const db = require("../models");
 
 const express = require("express");
 const router = express.Router();
 
-
 router.get("/workouts", (req, res) => {
     Exercise.find({}, function(err, data){
+        console.log("Bloop Attack!")
         if (err) {console.log(err)}
         else {
             console.log(data);
@@ -23,6 +23,7 @@ router.post("/workouts", (req, res) => {
         exercises: req.body.exercises
     });
     newWorkout.save();
+    // console.log(newWorkout);
 
     res.json(newWorkout);
 });
@@ -34,20 +35,38 @@ router.put("/workouts/:id", (req, res) => {
         type: req.body.type,
         name: req.body.name,
         duration: req.body.duration,
-        weight: req.body.duration,
+        weight: req.body.weight,
         reps: req.body.reps,
         sets: req.body.sets
     });
     newExercise.save();
+
+    // console.log(newExercise);
+
+    console.log(req.params.id);
     res.json(newExercise);
     
     Workout.findById(req.params.id, function(err, workout) {
+        console.log(workout);
 
             workout.exercises.push(newExercise.id);
+            console.log(newExercise);
+
             workout.save();
+            
+            console.log(workout);
             res.json(workout);
     });
 });
 
+// module.exports = function(app) {
+
+//     app.get("/workouts", function(req, res) {
+//         console.log("banana");
+//         db.Exercise.find({})
+//         .then()
+//     })
+
+//   };
 
 module.exports = router;
